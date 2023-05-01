c129610bee89d3db4932535d56d21ecbaf4d329d
volesh <volesh2@gmail.com>
2023-05-01 14:14:42 +0300

    Add env config and use port from .env file in the main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './modules/app/app.module';
+import { envConfig } from './general/configs/envConfig';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(envConfig.port);
 }
 bootstrap();
