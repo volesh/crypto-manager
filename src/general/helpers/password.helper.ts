848167359aaeca0383052eb12f5e7ce63751d548
volesh <volesh2@gmail.com>
2023-05-08 00:17:00 +0300

    fix bugs

--- src/general/helpers/password.helper.ts
+++ src/general/helpers/password.helper.ts
@@ -2,9 +2,12 @@ import * as bcrypt from 'bcrypt';
 
 export class PasswordHelper {
   static async hashPassword(password: string): Promise<string> {
-    return bcrypt.hashSync(password, 10);
+    return bcrypt.hashSync(password.trim(), 10);
   }
-  static async comparePassword(hashedPass, password): Promise<boolean> {
-    return bcrypt.compareSync(password, hashedPass);
+  static async comparePassword(
+    hashedPass: string,
+    password: string,
+  ): Promise<boolean> {
+    return bcrypt.compareSync(password.trim(), hashedPass);
   }
 }
